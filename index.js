15975a32a5af12f34abc5a11b2b254210a6ad0a0
crVainglory <cr_yang@yahoo.com>
2020-02-26 20:20:37 -0600

    demo ejs

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+const path = require('path')
 
 const indexRouter = require('./routes/index')
 const userRouter = require('./routes/users')
+
+// 存放模板引擎的目录
+app.set('views', path.join(__dirname, 'views'))
+// 设置模板引擎为 ejs
+app.set('view engine', 'ejs')
+
+
 
 app.use('/', indexRouter)
 app.use('/users', userRouter)
--- routes/users.js
+++ routes/users.js
@@ -2,7 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/:name', function (req, res) {
-  res.send('hello, ' + req.params.name)
+    // 第一个参数为模板名字， 第二个参数为数据
+    // <% code %>：运行 JavaScript 代码，不输出
+    // <%= code %>：显示转义后的 HTML内容
+    // <%- code %>：显示原始 HTML 内容
+  res.render('users', {
+      name: req.params.name
+  })
 })
 
 module.exports = router
